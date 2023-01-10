74434ea2bdab956dff3ee74455ac1f4f5d0efe2d
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2023-01-10 22:29:34 +0200

    Add project files.

---
+++ lab4ex6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab4ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 6 dificultate ridicata
+                Scrieti o functie recursiva care va calcula al n-lea element din sirul lui
+                Fibonacci, n fiind citit de la tastatura, apelati-o si afisati-I rezultatul
+             */
+
+            //Sir fibonacci: fiecare numar din sir => din suma celorlalte 2 numere fibo 
+
+            Console.WriteLine("introduceti N");
+            int n = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(GetFiboNthElem(n-1));
+        }
+
+        static int GetFiboNthElem(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            return GetFiboNthElem(n - 1) + GetFiboNthElem(n - 2);
+        }
+    }
+}
